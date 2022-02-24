413b67ce90a4bd421ce0eeae2d56c036be52a167
murat <cankaya.murat23@gmail.com>
2022-02-24 18:37:35 +0100

    dataset improve

--- src/data.py
+++ src/data.py
@@ -4,62 +4,91 @@ import matplotlib.pyplot as plt
 import json
 import xml.etree.ElementTree as ET
 import os
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+import torch
 
-class Data:
-    def __init__(self):
-        self.data_folder = "/home/inf4/Projects/aircraft_detection/Studenten"
-        json_file = open(f"{self.data_folder}/Gaofen/gaofen_sequences.json",'r')
+import cv2
 
-        self.training_json = json.load(json_file)
+class Data(Dataset):
+    def __init__(self,dataset_name='train'):
+        self.data_folder = "/home/murat/Projects/airplane_detection/DATA/Studenten/Gaofen"
+        self.dataset_name = dataset_name
+        self.json_file = json.load(open(f"{self.data_folder}/sequences.json",'r'))
 
-        # data folders
-        self.train_data_folder = f"{self.data_folder}/Gaofen/train"
-        self.val_data_folder = f"{self.data_folder}/Gaofen/val"
+        self.items = self.get_items()
 
+    def __len__(self):
+        return len(self.labels) 
 
-    def get_paths(self):
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-        train_img_paths = {}
-        val_img_paths = {}
-        for i in self.training_json:
+        img = cv2.imread(self.items[idx][0],1)
+        label = self.items[idx][1]
+        sample = {'image':img,'labels':label}
+        return sample
+
+    def get_items(self):
+        
+        items=[]
+
+        img_paths = self.get_img_paths()
+        labels = self.get_labels()
+
+        for img_name, img_path in img_paths.items():
+            label = labels[img_name]
+            items.append([img_path,label])
+        return items
+
+    def get_img_paths(self):
+        img_paths = {}
+        for i in self.json_file:
             image_dicts = i['images']
             for image_dict in image_dicts:
                 relative_path = image_dict['relative_path']
                 relative_path_split = relative_path.split('/')
                 dataset_part = relative_path_split[0]
                 img_name = relative_path_split[-1].split('.')[0]
-                if dataset_part == 'train':
-                    train_img_paths[img_name] = f"{self.train_data_folder}/{relative_path}"
-                elif dataset_part == 'val':
-                    val_img_paths[img_name] = f"{self.val_data_folder}/{relative_path}"
-        return train_img_paths, val_img_paths
+                if dataset_part == self.dataset_name:
+                    img_paths[img_name] = f"{self.data_folder}/{relative_path}"
+                    
+        return img_paths 
 
     def get_labels(self):
-        train_label_folder = f"{self.train_data_folder}/label_xml"
-        label_file_paths = [f"{train_label_folder}/{file_name}" for file_name in os.listdir(train_label_folder)]
+        labels={}
+
+        label_folder = f"{self.data_folder}/{self.dataset_name}/label_xml"
+        label_file_paths = [f"{label_folder}/{file_name}" for file_name in os.listdir(label_folder)]
 
 
-        root = ET.parse(label_file_paths[0]).getroot()
-        filename = root.findall('./source/filename')[0].text
+        for label_file_path in label_file_paths: 
+            root = ET.parse(label_file_path).getroot()
+            file_name = root.findall('./source/filename')[0].text
+            img_name = file_name.split('.')[0]
+            point_spaces = root.findall('./objects/object/points')        
+            points = []
 
-        point_spaces = root.findall('./objects/object/points')
-        
-        points = []
-        for point_space in point_spaces:
-            my_points = point_space.findall('point')[:4]
+            for point_space in point_spaces:
+                my_points = point_space.findall('point')[:4] # remove the last coordinate
 
-            coords = []
-            for my_point in my_points:
-                coord = []
-                for point in my_point.text.split(','):
-                    coord.append(float(point))
-                coords.append(coord)
-            points.append(coords)
+                coords = []
+                for my_point in my_points:
+                    # coord = []
+                    for point in my_point.text.split(','):
+                        coords.append(float(point))
+                    # coords.append(coord)
+                points.append(coords)
 
-        return points
+            labels[img_name] = points
+        return labels
+
 if __name__ == "__main__":
 
-    data = Data()
-    # train_img_paths, val_img_paths = data.get_paths()
-    # print(train_img_paths)
-    data.get_labels()
+    train_data = Data(dataset_name='test')
+    # print(len(train_data))
+    # print(train_data.get_labels())
+    # print(train_data.get_img_paths())
+    # print(train_data.items)
+    print(train_data[0])
---
+++ utils/temp.py
@@ -0,0 +1,36 @@
+import os
+
+### REPLACE 
+
+# Read in the file
+
+def replace_text(file_path,old_text,new_text):
+	with open(file_path, 'r') as file :
+	  filedata = file.read()
+
+	# Replace the target string
+	filedata = filedata.replace(old_text,new_text)
+
+	# Write the file out again
+	with open(file_path, 'w') as file:
+	  file.write(filedata)
+
+def get_file_name(file):
+	file_name = file.split('.')[0]
+	return file_name
+
+
+if __name__ == '__main__':
+	# old_text = f"A:{chr(92)}Datasets{chr(92)}GAOFEN{chr(92)}val{chr(92)}label_xml{chr(92)}"
+	# new_text = ''
+
+	my_folder = "/home/murat/Projects/airplane_detection/DATA/Studenten/Gaofen/val/label_xml"
+
+	for file in os.listdir(my_folder):
+		file_path = f"{my_folder}/{file}"
+
+		file_name = get_file_name(file)
+
+		old_text = f"{str(int(file_name)+1000)}.xml"
+		new_text = f"{file_name}.xml"
+		replace_text(file_path,old_text,new_text)
