8dcd2ae630f9b30bfb0da12c3d57f82d9b72c8ce
murat <cankaya.murat23@gmail.com>
2022-10-26 15:24:01 +0200

    big folder changes

--- src/data/cutout/geometry.py
+++ src/data/cutout/geometry.py
@@ -8,10 +8,11 @@ import cv2
 # TODO: Detection and recognition functionilities are confusing. Remove
 # class attributes, and call methods (not attributes) from
 # RecognitionPatch and DetectionPatch
-class Rectangle:
+class Bbox:
     '''
     This class does the followings:
-    - Parametrization of rectangle corner points, i.e., points >> center, height, length, rotation angle
+    - Parametrization of bbox corner points, i.e., corner points >> center_x, center_y, height, length, rotation angle
+    - Params to points, i.e.,  center_x, center_y, height, length, rotation angle >> corner points
     - Rotation calculations of bounding boxes
 
     '''
@@ -20,23 +21,29 @@ class Rectangle:
         if isinstance(bbox, list):
             bbox = np.array(bbox)
 
-        if bbox.shape != (4, 2):
-            print('bbox shape should be (4,2)')
+        if bbox.shape == (4, 2):
+            # self.bbox = bbox
+            self.bbox_corners = bbox
+            # self.cx, self.cy, self.h, self.w, self.angle = self.get_bbox_params()
+            self.bbox_params = self.get_bbox_params()
+            # self.orthogonal_bbox = self.get_orthogonal_bbox()
+
+        elif bbox.shape == (1, 5):
+            self.bbox_params = bbox
+
+        else:
+            raise Exception('bbox shape should be (4,2) for the corner definition OR (1,5) for the param definition')
             return 0
 
-        self.bbox = bbox
-        self.cx, self.cy, self.h, self.w, self.angle = self.get_params(
-            self.bbox)
+    def get_params(self):
+        cx = np.sum(self.bbox_corners[:, 0]) / 4.0
+        cy = np.sum(self.bbox_corners[:, 1]) / 4.0
 
-        self.orthogonal_bbox = self.get_orthogonal_bbox()
+        # def x_dif(i_0, i_1): return self.bbox_corners[i_0, 0] - self.bbox_corners[i_1, 0]
+        # def y_dif(i_0, i_1): return self.bbox_corners[i_0, 1] - self.bbox_corners[i_1, 1]
 
-    @staticmethod
-    def get_params(bbox):
-        cx = np.sum(bbox[:, 0]) / 4.0
-        cy = np.sum(bbox[:, 1]) / 4.0
-
-        def x_dif(i_0, i_1): return bbox[i_0, 0] - bbox[i_1, 0]
-        def y_dif(i_0, i_1): return bbox[i_0, 1] - bbox[i_1, 1]
+        x_dif = lambda (i_0, i_1): self.bbox_corners[i_0, 0] - self.bbox_corners[i_1, 0]
+        y_dif = lambda (i_0, i_1): self.bbox_corners[i_0, 1] - self.bbox_corners[i_1, 0]
 
         h = np.sqrt(x_dif(0, 1)**2 + y_dif(0, 1)**2)
         w = np.sqrt(x_dif(1, 2)**2 + y_dif(1, 2)**2)
@@ -44,23 +51,25 @@ class Rectangle:
         angle = np.arctan(y_dif(0, 1) / x_dif(0, 1))
         return [cx, cy, h, w, angle]
 
+    def get_corners(self):
+        
+
     def get_atan2(self):  # ,img_shape):
         '''
         Get the rotation matrix to rotate the airplane upwards
         '''
         # y_max, x_max, _ = img_shape
         # print(np.rad2deg(self.angle))
-        if (self.bbox[0, 1] - self.bbox[1, 1] >=
-                0) and (self.bbox[0, 0] - self.bbox[1, 0] >= 0):
+        if (self.bbox_corners[0, 1] - self.bbox_corners[1, 1] >=0) and (self.bbox_corners[0, 0] - self.bbox_corners[1, 0] >= 0):
             # print('q1')
             angle = 3 * np.pi / 2 + self.angle
-        elif (self.bbox[0, 1] - self.bbox[1, 1] >= 0) and (self.bbox[0, 0] - self.bbox[1, 0] <= 0):
+        elif (self.bbox_corners[0, 1] - self.bbox_corners[1, 1] >= 0) and (self.bbox_corners[0, 0] - self.bbox_corners[1, 0] <= 0):
             # print('q2')
             angle = np.pi / 2 + self.angle
-        elif (self.bbox[0, 1] - self.bbox[1, 1] <= 0) and (self.bbox[0, 0] - self.bbox[1, 0] <= 0):
+        elif (self.bbox_corners[0, 1] - self.bbox_corners[1, 1] <= 0) and (self.bbox_corners[0, 0] - self.bbox_corners[1, 0] <= 0):
             # print('q3')
             angle = np.pi / 2 + self.angle
-        elif (self.bbox[0, 1] - self.bbox[1, 1] <= 0) and (self.bbox[0, 0] - self.bbox[1, 0] >= 0):
+        elif (self.bbox_corners[0, 1] - self.bbox_corners[1, 1] <= 0) and (self.bbox_corners[0, 0] - self.bbox_corners[1, 0] >= 0):
             # print('q4')
             angle = 3 * np.pi / 2 + self.angle
         # M = cv2.getRotationMatrix2D((y_max/2, x_max/2), np.rad2deg(angle), 1.0)
@@ -73,9 +82,10 @@ class Rectangle:
         angle = -self.get_atan2()
         R = np.array([[np.cos(angle), -np.sin(angle)],
                       [np.sin(angle), np.cos(angle)]])
-        o = np.atleast_2d((self.cx, self.cy))
+        # o = np.atleast_2d((self.cx, self.cy))
+        o = np.atleast_2d((self.bbox_params[0], self.bbox_params[1]))
         # bbox = np.atleast_2d(self.bbox)
-        return np.squeeze((R @ (self.bbox.T - o.T) + o.T).T)
+        return np.squeeze((R @ (self.bbox_corners.T - o.T) + o.T).T)
 
     def get_orthogonal_bbox_by_limits(self, bbox, return_params):
         x_coords = bbox[:, 0]
@@ -94,7 +104,7 @@ class Rectangle:
                     [x_coord_min, y_coord_min]]
 
     @staticmethod
-    def plot_bbox(bbox, ax, c, s=15):
+    def plot_bbox(bbox_corners, ax, c, s=15):
         for i, coord in enumerate(bbox):
             # PLOT BBOX
             ax.plot([bbox[i - 1][0], coord[0]],
