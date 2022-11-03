ea34895d6e9b96f4f7abdda81214039a7a30a660
murat <cankaya.murat23@gmail.com>
2022-11-03 11:59:35 +0100

    conflic resolved

--- src/data/eval_mmrotate.py
+++ src/data/eval_mmrotate.py
@@ -8,18 +8,8 @@ import matplotlib.pyplot as plt
 from .cutout import geometry
 # from mmdet.apis.inference import init_detector, inference_detector, show_result_pyplot
 # import mmcv
-<<<<<<< Updated upstream
-from ast import literal_eval
-<<<<<<< Updated upstream
 import mmcv
-
-=======
->>>>>>> Stashed changes
-||||||| constructed merge base
-
-=======
 from ast import literal_eval
->>>>>>> Stashed changes
 
 class EvalMMRotate:
 	"""docstring for MMRotate"""
@@ -50,21 +40,11 @@ class EvalMMRotate:
 		print(df.iloc[2,2])
 		return df
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	def read_test_pkl(self):
 		test_pkl_path = self.settings['test_pkl_path']
 		pkl_object = mmcv.load(test_pkl_path)
 		print(pkl_object)
-=======
 
->>>>>>> Stashed changes
-
-||||||| constructed merge base
-=======
-
-
->>>>>>> Stashed changes
 	def show_image(self,ind,show_bbox=True):
 		## Read image path from settings (old) 
 		# test_images_path = self.settings['test_images_path']
