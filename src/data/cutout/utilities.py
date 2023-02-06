9d17e95a514994ef13c27b09051c714ac3c49a6d
louis <louis.niederloehner@gmail.com>
2023-02-06 13:01:06 +0100

    Changed from PIL to cv2 in utilities.py

--- src/data/cutout/utilities.py
+++ src/data/cutout/utilities.py
@@ -1,6 +1,7 @@
 import os
 
-from PIL import Image
+import cv2
+import numpy as np
 
 from src.data.cutout.cutout import Cutout
 
@@ -15,34 +16,24 @@ def filter_truncated_images(settings):
         for folder in ['image_folder', 'orthogonal_image_folder', 'orthogonal_zoomed_image_folder']:
 
             src_folder = settings['cutout'][dataset_part][folder]
-
-            folder_name = os.path.split(src_folder)[1]
-            folder_path = os.path.split(src_folder)[0]
-
             full_images_folder = settings['cutout'][dataset_part]['full_' + folder]
-
-            # truncated_images_folder = os.path.join(folder_path, folder_name + '_truncated_images')
-            # create_folder(truncated_images_folder)
-
             img_paths = Cutout.get_file_paths('', src_folder, False)
 
             for img_path in img_paths:
                 img_name = img_path.split('\\')[-1]
                 img_name = img_name.split('/')[-1]
                 print("Img_name: " + img_name)
-                print(img_path)
 
-                im = Image.open(img_path)
-                px = im.load()
-                width, height = im.size
+                im = cv2.imread(img_path)
+                width, height, x = im.shape
 
-                width_offset = width/3
-                height_offset = height/3
+                width_offset = int(width/3)
+                height_offset = int(height/3)
 
-                if ((px[0, 0] == (0, 0, 0) and px[width_offset, height_offset] == (0, 0, 0)) or
-                        (px[0, height - 1] == (0, 0, 0) and px[width_offset, height - height_offset] == (0, 0, 0)) or
-                        (px[width - 1, 0] == (0, 0, 0) and px[width - width_offset, height_offset] == (0, 0, 0)) or
-                        (px[width - 1, height - 1] == (0, 0, 0) and px[width - width_offset, height - height_offset] == (0, 0, 0))):
+                if ((np.array_equal(im[0, 0], [0, 0, 0]) and np.array_equal(im[width_offset, height_offset], [0, 0, 0])) or
+                        (np.array_equal(im[0, height - 1], [0, 0, 0]) and np.array_equal(im[width_offset, height - height_offset], [0, 0, 0])) or
+                        (np.array_equal(im[width - 1, 0], [0, 0, 0]) and np.array_equal(im[width - width_offset, height_offset], [0, 0, 0])) or
+                        (np.array_equal(im[width - 1, height - 1], [0, 0, 0]) and np.array_equal(im[width - width_offset, height - height_offset], [0, 0, 0]))):
                     print(f'Image {img_name} is truncated')
                 else:
-                    im.save(os.path.join(full_images_folder, img_name))
+                    cv2.imwrite(os.path.join(full_images_folder, img_name), im)
