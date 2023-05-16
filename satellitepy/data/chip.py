14ac2223d48f5567e2a1d5c6cece7d815dfe3058
Jonas Ruhnke <29842181+UnknownHobbyist@users.noreply.github.com>
2023-05-16 19:55:53 +0200

    updated chips.py according to the request

--- satellitepy/data/chip.py
+++ satellitepy/data/chip.py
@@ -24,7 +24,6 @@ def get_hbbox(bbox, margin_size, max_y, max_x):
 def get_chips(img, gt_labels, margin_size=100):
     height, width, channels = img.shape
     
-
     all_satellitepy_keys = get_all_satellitepy_keys()
 
     chips_dict = {
@@ -32,45 +31,28 @@ def get_chips(img, gt_labels, margin_size=100):
         'labels' : init_satellitepy_label()
     }
 
-    for i, bbox in enumerate(gt_labels['bboxes']):
+    bbox_type = ""
 
+    if "obboxes" in gt_labels:
+        bbox_type = "obboxes"
+    else:
+        bbox_type = "hbboxes"
+
+    bboxes = gt_labels[bbox_type]
+
+    for i, bbox in enumerate(bboxes):
         bbox = np.array(bbox)
+        hbbox = get_hbbox(bbox, margin_size, height, width)
 
-        if check_classes(gt_labels['classes']['0'][i], gt_labels['classes']['1'][i], gt_labels['classes']['2'][i]):
-            hbbox = get_hbbox(bbox, margin_size, height, width)
+        chip_img = create_chip(img, hbbox)
+        chip_bbox = bbox - [hbbox[0], hbbox[2]]
+        
+        set_chip_keys(all_satellitepy_keys, chips_dict['labels'], gt_labels, i)
 
-            chip_img = create_chip(img, hbbox)
-            chip_bbox = bbox - [hbbox[0], hbbox[2]]
-            
-            set_chip_keys(all_satellitepy_keys, chips_dict['labels'], gt_labels, i)
-            chips_dict['images'].append(chip_img)
+        chips_dict[bbox_type][-1] = chip_bbox 
+        chips_dict['images'].append(chip_img)
 
     return chips_dict
-
-def check_classes(class0, class1, class2):
-
-    instance_names = [
-        'Boeing787',
-        'Boeing737',
-        'Boeing747',
-        'Boeing777',
-        'A220',
-        'A321',
-        'A330',
-        'A350',
-        'ARJ21',
-        'C919',
-        'other-airplane'
-    ]
-    
-    if "plane" in class0:
-        return True
-    elif not class1 == None and class1 in instance_names:
-        return True
-    elif not class2 == None and any(name in class2 for name in instance_names):
-        return True
-    
-    return False
 
 
 def set_chip_keys(
