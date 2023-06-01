daf3dcb94280634753f0f13118d9986448131123
Sascha Köjer <sascha.koejer@unibw.de>
2023-06-01 12:37:22 +0200

    added configparser

--- satellitepy/data/tools.py
+++ satellitepy/data/tools.py
@@ -4,6 +4,7 @@ import cv2
 from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 
 from satellitepy.utils.path_utils import create_folder, get_file_paths, zip_matched_files
 from satellitepy.data.patch import get_patches
@@ -84,11 +85,9 @@ def save_patches(
                 json.dump(patch_label,f)
 
 
-def show_labels_on_image(img_path,label_path,label_format,show_labels=True):
-    # img_path = os.path.join(self.original_image_folder,f'{img_name}.tif')
-    # print(img_path)
-    # bbox_path = os.path.join(self.original_bbox_folder,f'{img_name}.xml')
-    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+def show_labels_on_image(img_path,label_path,label_format,output_folder,show_bboxes):
+    logger = logging.getLogger(__name__)
+    img = cv2.cvtColor(cv2.imread(str(img_path)), cv2.COLOR_BGR2RGB)
     gt_labels = read_label(label_path,label_format)
 
     # fig, ax = plt.subplots(1)
@@ -99,14 +98,16 @@ def show_labels_on_image(img_path,label_path,label_format,show_labels=True):
     fig.add_axes(ax)
     ax.imshow(img)
     # ax.imshow(mask, alpha=0.5)
-
-    for bbox in gt_labels['bboxes']:
-        bbox_corners = np.array(bbox[:8]).astype(int).reshape(4, 2)
-        BBox.plot_bbox(corners=bbox_corners, ax=ax, c='b', s=5, instance_name=None)
+    if show_bboxes:
+        for bbox in gt_labels['bboxes']:
+            bbox_corners = np.array(bbox[:8]).astype(int).reshape(4, 2)
+            BBox.plot_bbox(corners=bbox_corners, ax=ax, c='b', s=5, instance_name=None)
     plt.axis('off')
     # manager = plt.get_current_fig_manager()
     # manager.window.showMaximized()
     plt.show()
+    plt.savefig(output_folder / Path(img_path.stem + ".png"))
+    logger.info(f'Saved labels on {output_folder / Path(img_path.stem + ".png")}')
     return fig
 
 
--- tools/data/display_labels_on_image.py
+++ tools/data/display_labels_on_image.py
@@ -7,16 +7,46 @@ import logging
 Show labels (e.g., bounding boxes) on an image
 """
 
-# TODO: 
-#	Accept arguments from terminal
+project_folder = get_project_folder()
 
+def get_args():
+    """Arguments parser."""
+    parser = configargparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--image-path', type=Path, required=True,
+                        help='Image that should be displayed')
+    parser.add_argument('--label-path', type=Path, required=True,
+                        help='Label that corresponds to the given image')
+    parser.add_argument('--label-format', type=str, required=True,
+                        help='Label file format. e.g., dota, fair1m.')
+    parser.add_argument('--output-folder', type=Path, required=False, default= project_folder / Path('docs/labels_on_images/'),
+                        help='Folder where the generated image should be saved to. Default: satellitepy/docs/labels_on_images')
+    parser.add_argument('--show-bboxes', type=str, default='True', required=False,
+                        help='Whether to show bounding boxes on the generated image')
+    parser.add_argument('--log-config-path', default=project_folder /
+                        Path("configs/log.config"), type=Path, help='Log config file.')
+    parser.add_argument('--log-path', type=Path, default=None, help='Log file path.')
+    args = parser.parse_args()
+    return args
+
+
+def run(args):
+    image_path = Path(args.image_path)
+    label_path = Path(args.label_path)
+    label_format = str(args.label_format)
+    output_folder = Path(args.output_folder)
+    assert create_folder(output_folder)
+    show_bboxes = args.show_bboxes.lower() == 'true'
+    log_path = output_folder / f'display_labels.log' if args.log_path == None else args.log_path
+
+    init_logger(config_path=args.log_config_path, log_path=log_path)
+    logger = logging.getLogger(__name__)
+    logger.info(
+        f'No log path is given, the default log path will be used: {log_path}')
+    
+    logger.info(f'Displaying labels on {image_path.name}')
+
+    show_labels_on_image(image_path,label_path,label_format,output_folder,show_bboxes)
+    
 if __name__ == '__main__':
-    # args = get_args()
-    # run(args)
-	# img_path = '/home/murat/Projects/satellitepy/data/fair1m/train/images/16483.tif'
-	img_path = '/home/murat/Projects/satellitepy/data/fair1m/train/images/16487.tif'
-	
-	label_path = '/home/murat/Projects/satellitepy/data/fair1m/train/bounding_boxes/16487.xml'
-	label_format = 'fair1m'
-
-	show_labels_on_image(img_path,label_path,label_format)
+    args = get_args()
+    run(args)
