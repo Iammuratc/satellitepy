671d7413b2c598f6ba42880f35a8f7393d9fc8c3
Sascha Köjer <sascha.koejer@unibw.de>
2023-06-06 17:12:11 +0200

    added tasks parameter and an error message if folders for create patch have unequal length

--- tools/data/display_labels_on_image.py
+++ tools/data/display_labels_on_image.py
@@ -20,8 +20,8 @@ def get_args():
                         help='Label file format. e.g., dota, fair1m.')
     parser.add_argument('--output-folder', type=Path, required=False, default= project_folder / Path('docs/labels_on_images/'),
                         help='Folder where the generated image should be saved to. Default: satellitepy/docs/labels_on_images')
-    parser.add_argument('--show-bboxes', type=str, default='True', required=False,
-                        help='Whether to show bounding boxes on the generated image')
+    parser.add_argument('--tasks', type=str, required=False, nargs='+',
+                        help='Which information to show on genertet image. E.g.: bboxes, masks, labels')
     parser.add_argument('--log-config-path', default=project_folder /
                         Path("configs/log.config"), type=Path, help='Log config file.')
     parser.add_argument('--log-path', type=Path, default=None, help='Log file path.')
@@ -35,7 +35,10 @@ def run(args):
     label_format = str(args.label_format)
     output_folder = Path(args.output_folder)
     assert create_folder(output_folder)
-    show_bboxes = args.show_bboxes.lower() == 'true'
+    tasks = args.tasks
+
+    if tasks == None:
+        tasks = ['bboxes', 'classes_0']
     log_path = output_folder / f'display_labels.log' if args.log_path == None else args.log_path
 
     init_logger(config_path=args.log_config_path, log_path=log_path)
@@ -45,7 +48,7 @@ def run(args):
     
     logger.info(f'Displaying labels on {image_path.name}')
 
-    show_labels_on_image(image_path,label_path,label_format,output_folder,show_bboxes)
+    show_labels_on_image(image_path,label_path,label_format,output_folder,tasks)
     
 if __name__ == '__main__':
     args = get_args()
