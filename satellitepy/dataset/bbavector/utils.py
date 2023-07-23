Removed augmentation from Dataset for evaluation, fixed rotation for creating results for bbavector

satellitepy/dataset/bbavector/utils.py
@@ -17,12 +17,14 @@ class Utils:
         self.max_objs = 500
         self.image_distort =  data_augment.PhotometricDistort()
 
-    def data_transform(self, image, annotation):
+    def data_transform(self, image, annotation, augmentation):
         # only do random_flip augmentation to original images
         crop_size = None
         crop_center = None
-        crop_size, crop_center = random_crop_info(h=image.shape[0], w=image.shape[1])
-        image, annotation['pts'], crop_center = random_flip(image, annotation['pts'], crop_center)
+
+        if augmentation:
+            crop_size, crop_center = random_crop_info(h=image.shape[0], w=image.shape[1])
+            image, annotation['pts'], crop_center = random_flip(image, annotation['pts'], crop_center)
         if crop_center is None:
             crop_center = np.asarray([float(image.shape[1])/2, float(image.shape[0])/2], dtype=np.float32)
         if crop_size is None:
@@ -31,7 +33,7 @@ class Utils:
                                crop_size=crop_size,
                                dst_size=(self.input_w, self.input_h),
                                inverse=False,
-                               rotation=True)
+                               rotation=augmentation)
         image = cv2.warpAffine(src=image, M=M, dsize=(self.input_w, self.input_h), flags=cv2.INTER_LINEAR)
         if annotation['pts'].shape[0]:
             annotation['pts'] = np.concatenate([annotation['pts'], np.ones((annotation['pts'].shape[0], annotation['pts'].shape[1], 1))], axis=2)

satellitepy/evaluate/bbavector/tools.py
@@ -80,7 +80,8 @@ def save_patch_results(
         task_dict,
         input_h,
         input_w,
-        down_ratio)
+        down_ratio,
+        False)
     # Dataloader
     data_loader = torch.utils.data.DataLoader(dataset,
         batch_size=1,

satellitepy/models/bbavector/utils.py
@@ -55,7 +55,7 @@ def decode_predictions(predictions, orig_h, orig_w, input_h, input_w, down_ratio
         br = bb + rr - cen_pt
         score = pred[10]
         clse = pred[11]
-        pts = np.asarray([tr, br, bl, tl], np.float32)
+        pts = np.asarray([tl, bl,br, tr,], np.float32)
         pts[:, 0] = pts[:, 0] * down_ratio / input_w * orig_w
         pts[:, 1] = pts[:, 1] * down_ratio / input_h * orig_h
         # pts0[dsets.category[int(clse)]].append(pts)