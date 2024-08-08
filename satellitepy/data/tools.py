c911d09ab195f414753b265b8c95cd44218d0895
philipphardy@gmail.com <philipphardy@gmail.com>
2024-08-08 11:17:49 +0200

    subset-csv

--- satellitepy/data/tools.py
+++ satellitepy/data/tools.py
@@ -88,7 +88,8 @@ def save_patches(
         mask_folder=None,
         rescaling=1,
         interpolation_method=cv2.INTER_LINEAR,
-        keep_empty=False
+        keep_empty=False,
+        subset_data=None
     ):
     """
     Save patches from the original images
@@ -126,8 +127,12 @@ def save_patches(
 
     assert out_folder.exists(), 'Make sure the out-folder exists or is created before calling this function.'
 
-    out_image_folder = Path(out_folder) / 'images'
-    out_label_folder = Path(out_folder) / 'labels'
+    if subset_data is not None:
+        out_image_folder = Path(out_folder)
+        out_label_folder = Path(out_folder)
+    else:
+        out_image_folder = Path(out_folder) / 'images'
+        out_label_folder = Path(out_folder) / 'labels'
 
     assert create_folder(out_image_folder, ask_permission=False)
     img_paths = get_file_paths(image_folder)
@@ -147,6 +152,15 @@ def save_patches(
         mask_name = mask_path.name if mask_path != None else None
         label_name = label_path.name if label_path is not None else None
         logger.info(f"{img_path.name}, {label_name}, {mask_name}")
+
+        # Change output directory to patches if subset_data is not None
+        if subset_data is not None:
+            subset = subset_data.loc[subset_data['image'] == '/mnt/2tb-1/satellitepy/data/FR24_dataset/images/' + img_path.name]
+            if not subset.empty:
+                out_image_folder = Path(out_image_folder) / subset.values[0][2] / 'images'
+                assert create_folder(out_image_folder)
+                out_label_folder = Path(out_label_folder) / subset.values[0][2] / 'labels'
+                assert create_folder(out_label_folder)
 
         gt_labels = read_label(label_path, label_format, mask_path, rescaling)
 
